a694fe3fcc362ba31657b7b3dc0fefdb1ca5338a
Michael Hueschen <m@mhueschen.space>
2022-07-17 04:08:46 -0600

    add rust-overlay/cargo2nix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,28 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    cargo2nix.url = "github:cargo2nix/cargo2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, cargo2nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [
+          rust-overlay.overlays.default
+          cargo2nix.overlays.default
+        ];
+        pkgs = import nixpkgs {
+          inherit overlays system;
+        };
       in
-      with pkgs;
       {
-        devShell = mkShell {
+        devShell = with pkgs; mkShell {
           buildInputs = [
             wasm
             wabt
+            rust-bin.stable."1.61.0".default
+            cargo2nix.packages.${system}.default
           ];
         };
       });
